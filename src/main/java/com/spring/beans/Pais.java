d1bf33e9c7ce4c2630f0e635c400385d2d94d312
Leonardo Suarez <suarezgl@gmail.com>
2017-11-20 00:02:42 -0500

    Inyeccion de Objetos por referencia

---
+++ src/main/java/com/spring/beans/Pais.java
@@ -0,0 +1,26 @@
+package com.spring.beans;
+
+
+public class Pais {
+
+	private String nombre;
+	private Ciudad ciudad;
+	
+
+	public Ciudad getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(Ciudad ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+}
---
+++ src/main/java/com/spring/beans/Persona.java
@@ -0,0 +1,52 @@
+package com.spring.beans;
+
+public class Persona {
+
+	private int id;
+	private String nombre;
+	private String apodo;
+	private Pais pais;
+
+	
+	
+	public Pais getPais() {
+		return pais;
+	}
+
+
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApodo() {
+		return apodo;
+	}
+
+	public void setApodo(String apodo) {
+		this.apodo = apodo;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Persona [id=" + id + ", nombre=" + nombre + ", apodo=" + apodo + " Pais" + this.getPais().getNombre() + " Ciudad "+  this.getPais().getCiudad().getNombre()+" ]";
+	}
+	
+	
+}
